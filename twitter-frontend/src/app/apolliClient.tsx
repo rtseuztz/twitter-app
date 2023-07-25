5e80e44a10856374e1a54170e30d94cc6ccbd0e9
Saji Shunnarah <sajishunnarah@gmail.com>
2023-07-25 13:30:30 -0400

    idiot sandwich mode

--- twitter-frontend/src/app/apolliClient.tsx
+++ twitter-frontend/src/app/apolliClient.tsx
@@ -8,7 +8,7 @@ export default function ApolloClientWrapper({
 }) {
     console.log("server url: ", process.env.NEXT_PUBLIC_SERVER_URL, `${process.env.NEXT_PUBLIC_SERVER_URL}/api/graphql/`)
     const client = new ApolloClient({
-        uri: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/graphql/`,
+        uri: `api/graphql/`,
         cache: new InMemoryCache(),
     });
     return (
